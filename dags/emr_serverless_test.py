27e7fa844409c3fc454b44d2313fc417f2d056ec
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-04-23 19:00:35 -0400

    added outline for EMR serverless spark job

--- dags/emr_serverless_test.py
+++ dags/emr_serverless_test.py
@@ -2,9 +2,14 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.providers.amazon.aws.operators.emr import (
     EmrServerlessCreateApplicationOperator,
+    EmrServerlessStartJobOperator,
     EmrServerlessDeleteApplicationOperator
 )
 
+# Define the S3 path to the Spark job JAR
+spark_job_s3_path = 's3://afarhidev-private-jars/artifacts/location-iq/location-summary-etl-LATEST.jar'
+
+# Define the DAG
 with DAG(
     'emr_serverless_dag',
     default_args={
@@ -16,31 +21,39 @@ with DAG(
         'retries': 0,
     },
     description='Run Spark job on EMR Serverless',
-    schedule=timedelta(days=7),
+    schedule_interval=timedelta(days=7),
     start_date=datetime(2023, 4, 18),
     catchup=False,
 ) as dag:
 
-    # TODO: try triggering spark jar that is in private s3 bucket
+    # Create the EMR Serverless application
     create_serverless_app = EmrServerlessCreateApplicationOperator(
         task_id="create_emr_serverless_app",
         release_label="emr-6.6.0",
         job_type="SPARK",
         config={
-            "name": "new_application",
-            "sparkSubmitJobDriver": {
-                "entryPoint": "s3://<your-bucket>/<path-to>/your-spark-job.jar",
-                "entryPointArguments": ["arg1", "arg2"],
-                "sparkSubmitParameters": "--conf spark.executorEnv.YOUR_ENV_VAR=your_env_value"
-            }
+            "name": "new_application"
         },
         aws_conn_id="my_aws_connection"
     )
 
+    # Start Spark job
+    run_spark_job = EmrServerlessStartJobOperator(
+        task_id="run_location_summary_etl_job",
+        application_id="{{ task_instance.xcom_pull(task_ids='create_emr_serverless_app', key='return_value') }}",
+        job_driver={
+            "sparkSubmit": {
+                "entryPoint": spark_job_s3_path,
+            }
+        },
+    )
+
+    # Delete the EMR Serverless application
     delete_app = EmrServerlessDeleteApplicationOperator(
-        task_id="delete_app",
+        task_id="delete_emr_serverless_app",
         aws_conn_id="my_aws_connection",
         application_id="{{ task_instance.xcom_pull(task_ids='create_emr_serverless_app', key='return_value') }}",
     )
 
-    create_serverless_app >> delete_app
+    # Define the DAG dependencies
+    create_serverless_app >> run_spark_job  >> delete_app
